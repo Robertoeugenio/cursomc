5e5f49dc791cc73c4030c67a96b185b37ed3bc65
robertoeugenio1978@gmail.com <robertoeugenio>
2018-08-30 22:07:31 -0300

    Estado e cidade

--- src/main/java/com/robertoeugenio/cursomc/CursomcApplication.java
+++ src/main/java/com/robertoeugenio/cursomc/CursomcApplication.java
@@ -8,40 +8,62 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.robertoeugenio.cursomc.domain.Categoria;
+import com.robertoeugenio.cursomc.domain.Cidade;
+import com.robertoeugenio.cursomc.domain.Estado;
 import com.robertoeugenio.cursomc.domain.Produto;
 import com.robertoeugenio.cursomc.repositories.CategoriaRepository;
+import com.robertoeugenio.cursomc.repositories.CidadeRepository;
+import com.robertoeugenio.cursomc.repositories.EstadoRepository;
 import com.robertoeugenio.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-	
-	@Autowired //para inicicar salvando 
+
+	@Autowired // para inicicar salvando
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtorepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");//  instanciados ]
-		
-		Produto p1 = new Produto (null, "Computador", 2000.00);
-		Produto p2 = new Produto (null, "Impressora", 800.00);
-		Produto p3 = new Produto (null, "Mouse", 80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));   //fazendo as associaçoes de mao dupla
+		Categoria cat2 = new Categoria(null, "Escritório");// instanciados ]
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3)); // fazendo as associaçoes de mao dupla
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-			
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));   // fazendo as associaçoes de mao dupla
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1)); // fazendo as associaçoes de mao dupla
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));// criar lista automatica com elementos que eu quiser
+		produtorepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));//criar lista automatica com elementos que eu quiser
-		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+
 	}
 }
