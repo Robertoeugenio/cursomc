de0b8c790cdaaa17815216913f6e95e78b1baf77
robertoeugenio1978@gmail.com <robertoeugenio>
2018-09-13 19:54:26 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/robertoeugenio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/robertoeugenio/cursomc/resources/CategoriaResource.java
@@ -42,4 +42,9 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build(); //conteudo  vazio
 	}
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {// tipo especial armazena informaçoes
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/robertoeugenio/cursomc/services/CategoriaService.java
+++ src/main/java/com/robertoeugenio/cursomc/services/CategoriaService.java
@@ -3,33 +3,45 @@ package com.robertoeugenio.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.robertoeugenio.cursomc.domain.Categoria;
 import com.robertoeugenio.cursomc.repositories.CategoriaRepository;
+import com.robertoeugenio.cursomc.services.exceptions.DataIntegrityException;
 import com.robertoeugenio.cursomc.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
- 
-	public Categoria find (Integer id) {
-	 Optional <Categoria>obj = repo.findById(id);
-	
-		 return obj.orElseThrow(() -> new ObjectNotFoundException(
-				 "Objeto não Encontrado Id: " + id + " , Tipo " + Categoria.class.getName()));
-		 
-	 }
-		public Categoria insert(Categoria obj) {
-			obj.setId(null);    //esse metodo para garantir que o objeto que vai ser inserido seja nulo
-			return repo.save(obj);
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não Encontrado Id: " + id + " , Tipo " + Categoria.class.getName()));
+
+	}
+
+	public Categoria insert(Categoria obj) { // metodo para inserir
+		obj.setId(null); // esse metodo para garantir que o objeto que vai ser inserido seja nulo
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) { // metodo para atualizar
+		find(obj.getId()); // ele busca e se nao houver ele lança uma recessao
+		return repo.save(obj); // atualizar.....
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) { // mensagem quando for deletar 
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+
 		}
-		public Categoria update(Categoria obj) {
-			find(obj.getId()); //ele busca e se nao houver ele lança uma recessao
-			return repo.save(obj); // atualizar.....
-		}
+	}
 }
-	 
