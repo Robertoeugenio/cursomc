ef2c7e55ce5779cfe5952fa89b244fbf03ba6850
robertoeugenio1978@gmail.com <robertoeugenio>
2018-09-13 22:21:25 -0300

    Paginacao cm parametros opcionais na requisicao

--- src/main/java/com/robertoeugenio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/robertoeugenio/cursomc/resources/CategoriaResource.java
@@ -2,15 +2,16 @@ package com.robertoeugenio.cursomc.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -21,6 +22,7 @@ import com.robertoeugenio.cursomc.services.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
 	@Autowired
 	private CategoriaService service;
 
@@ -35,30 +37,39 @@ public class CategoriaResource {
 																		// //requestbody faz ser convertido para objeto
 																		// categoria automaticamente
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();// caracteristica
-																														// do
-																														// framework
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();// caracteristica																														// do																												// framework
 		return ResponseEntity.created(uri).build();
 	}
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT) //requisição feito no put
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT) // requisição feito no put
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
-		return ResponseEntity.noContent().build(); //conteudo  vazio
+		return ResponseEntity.noContent().build(); // conteudo vazio
 	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {// tipo especial armazena informaçoes
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
 	@RequestMapping(method = RequestMethod.GET) // nao precisa de id pois vai buscar todas as categorias
 	public ResponseEntity<List<CategoriaDTO>> findAll() {// tipo especial armazena informaçoes
-		List<Categoria> list = service.findAll(); //vai buscar uma lista por isso o metodo é de lista
-		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());//converter uma lista para outra lista
-		return ResponseEntity.ok().body(listDto); //retornar lista de categorias
+		List<Categoria> list = service.findAll(); // vai buscar uma lista por isso o metodo é de lista
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());// converter lista para outra lista 																																																					
+		return ResponseEntity.ok().body(listDto); // retornar lista de categorias
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET) // nao precisa de id pois vai buscar todas as categorias
+
+	public ResponseEntity<Page<CategoriaDTO>> findPage( // tipo especial armazena informaçoes
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction); // vai buscar uma lista por isso o metodo é de lista 																		
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj)); // converter uma lista para outra lista
+		return ResponseEntity.ok().body(listDto); // retornar lista de categorias
 	}
 }
-
-
-
-
